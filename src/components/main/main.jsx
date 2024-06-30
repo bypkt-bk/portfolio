780eba5ec311421aba626791601a5a438edfc65d
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2024-07-01 05:45:56 +0700

    update

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,10 +3,8 @@ function Main() {
     return (
         <>  
             <div className="center"><div className="circle"></div></div>
-            <div className="info">
                 <div className='Name'>BANYAPHON KONGTHAM</div>
                 <div className="study">computer science'student at chulalongorn university</div>
-            </div>
 
         </>
     )
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -7,12 +7,7 @@ function Profile() {
                 <img src='src/assets/IMG_4552.JPG' alt='boom'>
             </img>
                 <div className="general-info">
-                    <div className="detail"><p-ui>My name is Banyaphon Kongtham, a student at Chulalongkorn University's Faculty of Science,
-                        Department of Mathematics and Computer Science. In addition to my studies,
-                        I also gain practical experience working with the Science Student Committee's IT department.
-                        UX/UI has always fascinated me! This is a great opportunity for me to develop my knowledge and
-                         skills in something I'm passionate about. When I worked with Figma, I was completely captivated by design work and
-                          I believe I'm on the right track.</p-ui></div>
+                    <div className="detail"><p-ui>I am Banyaphon Kongtham, a Mathematics and Computer Science student at Chulalongkorn University. I work with the Science Student Committee's IT department and am passionate about UX/UI design, especially using Figma.</p-ui></div>
                     <div className="info"><p-ui>Name: Banyaphon Kongtham<br/>
                         Birthday: Feb 8, 2004<br/>
                         Marital: Single<br/>
